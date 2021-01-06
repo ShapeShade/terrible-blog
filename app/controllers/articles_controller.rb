97120e1c1b8dd92adc57053b96381de4bc96bef9
Daniel Plambeck <Shade@ProjectDarkness.com>
2021-01-06 13:22:17 +0100

    Continuing ArticlesController & view, current action :new :create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,25 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Successfully Created new article'
+      redirect_to(article_path(@article))
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
