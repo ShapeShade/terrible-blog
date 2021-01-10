784eec680cb6dee66f735004c47effe6c55100f5
Daniel Plambeck <Shade@ProjectDarkness.com>
2021-01-10 23:30:38 +0100

    Something about User validations?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
+# User model
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
+
+  validates :username, presence: true, length: { within: 3..50 }, uniqueness: { case_sensitive: false }
+
+  validates :email, presence: true,
+                    length: { within: 3..128 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
 end
