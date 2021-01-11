f6d1ac6e4b8c3fbd998826659200b23c20f24a29
Daniel Plambeck <Shade@ProjectDarkness.com>
2021-01-12 00:40:07 +0100

    Fixed some weak warnings in sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,25 +2,23 @@
 
 # Sessions Controller
 class SessionsController < ApplicationController
-  def new
-
-  end
+  def new; end
 
   def create
     user = User.find_by(username: params[:session][:username].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Logged in successfully."
+      flash[:notice] = 'Logged in successfully.'
       redirect_to(user)
     else
-      flash.now[:alert] = "Login Failed. (might have been wrong username / password combo)"
+      flash.now[:alert] = 'Login Failed. (might have been wrong username / password combo)'
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Logged out."
+    flash[:notice] = 'Logged out.'
     redirect_to(root_path)
   end
 end
