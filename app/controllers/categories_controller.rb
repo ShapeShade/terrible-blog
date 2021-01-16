f9b103420217a08ba71626bcba1cc3d5e844c01b
Daniel Plambeck <Shade@ProjectDarkness.com>
2021-01-17 00:19:17 +0100

    Edit category name now functional

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,6 +24,20 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      flash[:notice] = "Category Name updated successfully."
+      redirect_to(@category)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def category_params
